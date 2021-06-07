af7ebec961c79790f438f991ae2927704bef5dd3
MarytereBenavides <75746438+MarytereBenavides@users.noreply.github.com>
2021-06-07 09:50:16 -0500

    ADD: SCSS FILES

--- src/modules/auth/screens/AuthLoginScreen.js
+++ src/modules/auth/screens/AuthLoginScreen.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import '../../../scss/auth.scss';
+import logo from '../../../assets/images/LogoBurger.png';
 
 const AuthLoginScreen = () => {
  return (
-     <div>Hola login</div>
+     <div>
+         <header>
+             <img src={logo} width='973' height='191' alt='logo' />
+         </header>
+         <main>
+            <form>
+                <label>Username</label>
+                <input type="email" placeholder='Enter gmail'></input>
+
+                <label>Password</label>
+                <input type="password" placeholder='***********'></input>
+                <div>
+                    <button className='buttonLogin' type="submit">Submit</button>
+                </div>
+            </form>
+         </main>
+     </div>
  )
 }
 
