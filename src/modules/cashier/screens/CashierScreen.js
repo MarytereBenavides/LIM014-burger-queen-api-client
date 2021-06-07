7d45aaffbaa393349a68e52cfa794fdafe6160b7
MarytereBenavides <75746438+MarytereBenavides@users.noreply.github.com>
2021-06-07 16:56:53 -0500

    ADD: CashierScreen

--- src/modules/cashier/screens/CashierScreen.js
+++ src/modules/cashier/screens/CashierScreen.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
-const CashierScreen = () => {
- return(
-     <div>Hola screen cashier</div>
- )
+
+const formReducer = (state, event) => {
+ return {
+   ...state,
+   [event.name]: event.value
+ }
 }
 
-export default CashierScreen
+function CashierScreen() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  return(
+    <div >
+        <div>
+            <label>CLIENT :</label>
+            <input name="name" onChange={handleChange}/>
+        </div>
+       <div>
+         <ul>
+           {Object.entries(formData).map(([name, value]) => (
+             <p key={name}>{value.toString()}</p>
+           ))}
+         </ul>
+       </div>
+       
+    </div>
+
+  )
+}
+
+export default CashierScreen;
