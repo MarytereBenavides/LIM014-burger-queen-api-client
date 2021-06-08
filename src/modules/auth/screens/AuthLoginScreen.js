e1a703e70c3f92b4133db162dacfcbb3d4e140a9
Denisse Marinel Montalvo Castillo <marinel.casti@gmail.com>
2021-06-08 02:30:27 -0500

    style: create style of login

--- src/modules/auth/screens/AuthLoginScreen.js
+++ src/modules/auth/screens/AuthLoginScreen.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import '../../../scss/auth.scss';
-import logo from '../../../assets/images/LogoBurger.png';
+import React from "react";
+import "../../../scss/auth.scss";
+import logo from "../../../assets/images/LogoBurger.png";
 
 const AuthLoginScreen = () => {
- return (
-     <div>
-         <header>
-             <img src={logo} width='973' height='191' alt='logo' />
-         </header>
-         <main>
-            <form>
-                <label>Username</label>
-                <input type="email" placeholder='Enter gmail'></input>
+  return (
+    <div className="container">
+      <header className="container__header">
+        <img src={logo} width="973" height="191" alt="logo" />
+      </header>
+      <main className="container__login">
+        <form className="container__login-form">
+          <label className="form-label">Gmail</label>
+          <input className="form-input" type="email" placeholder="user@gmail.com"></input>
+          <label className="form-label">Password</label>
+          <input type="password" placeholder="***********" className="form-input" ></input>
+          <div>
+            <button className="form-btnLogin" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
+};
 
-                <label>Password</label>
-                <input type="password" placeholder='***********'></input>
-                <div>
-                    <button className='buttonLogin' type="submit">Submit</button>
-                </div>
-            </form>
-         </main>
-     </div>
- )
-}
-
-export default AuthLoginScreen
+export default AuthLoginScreen;
